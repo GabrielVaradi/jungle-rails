f1631c9e6cc7b456b4cbbceac66d1ca4a588b5ff
Gabriel Varadi <gabriel.varadi.hallope.lecoq@gmail.com>
2019-06-13 14:40:39 +0000

    Make the project more modular with helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,49 @@
 module ApplicationHelper
+
+  def rating(product)
+
+  end
+
+  def logged_in?(session)
+    if session["user_id"]
+      true
+    else
+      false
+    end
+  end
+
+  def is_same_user?(session, content)
+    if session["user_id"] == content.user_id
+      true
+    else
+      false
+    end
+  end
+
+  def is_review_empty?(product)
+    if product.reviews.size == 0
+      true
+    else
+      false
+    end
+  end
+
+  def out_of_stock?(product)
+    if product.quantity == 0
+      true
+    else
+      false
+    end
+
+  end
+
+  def is_cart_empty?(cart)
+    if cart.size == 0
+      true
+    else
+      false
+    end
+  end
+
+
 end
